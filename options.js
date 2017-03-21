20920ad690c8bf50d91c600a32423147d7ad048d
王镇 <hero21c@outlook.com>
2017-03-21 23:42:10 +0800

    基本 完成bookmark转markdown的功能

--- options.js
+++ options.js
@@ -1,123 +1,131 @@
 pathLookup = {};
 selectedId = -1;
 
-Extension = function() {
+Extension = function () {
 };
 
 Extension.settings = {
-    'logging': true
+  'logging': true
 };
 
-Extension.log = function(msg) {
-    if(this.settings.logging) {
-        console.log(arguments);
-    }
+Extension.log = function (msg) {
+  if (this.settings.logging) {
+    console.log(arguments);
+  }
 };
 
-function getBookmarkNodeType(BookmarkTreeNode) {
-    if(BookmarkTreeNode.hasOwnProperty('url')) {
-        type = "Bookmark";
-    } else {
-        type = "Folder";
-    }
-    return type;
+function getBookmarkNodeType(bookmarkTreeNode) {
+  if (bookmarkTreeNode === undefined) return ''
+  return bookmarkTreeNode.hasOwnProperty('url') ? "Bookmark" : "Folder"
 }
 
 function populateBookmarkOptions(BookmarkTreeNodeArray, depth, folders, folderLabel) {
 
-    if(typeof depth === 'undefined') {
-        depth = 0;
-        folders = {};
-        folderLabel = "";
+  if (typeof depth === 'undefined') {
+    depth = 0;
+    folders = {};
+    folderLabel = "";
+  }
+
+  for (var x = 0; x < BookmarkTreeNodeArray.length; x++) {
+    var BookmarkTreeNode = BookmarkTreeNodeArray[x];
+
+    var type = getBookmarkNodeType(BookmarkTreeNode);
+
+    if (type === "Folder") {
+      const children = BookmarkTreeNode.children;
+
+      if (depth === 0) {
+        folderPath = BookmarkTreeNode.title;
+      } else {
+        folderPath = folderLabel + "/" + BookmarkTreeNode.title;
+      }
+
+      folders[folderPath] = BookmarkTreeNode.id;
+      pathLookup[BookmarkTreeNode.id + ""] = folderPath;
+      populateBookmarkOptions(children, depth + 1, folders, folderPath);
     }
+  }
 
-    for(var x=0; x<BookmarkTreeNodeArray.length; x++) {
-        var BookmarkTreeNode = BookmarkTreeNodeArray[x];
-
-        var type = getBookmarkNodeType(BookmarkTreeNode);
-
-        if(type === "Folder") {
-            var children = BookmarkTreeNode.children;
-
-            if(depth === 0) {
-                folderPath = BookmarkTreeNode.title;
-            } else {
-                folderPath = folderLabel + "/" + BookmarkTreeNode.title;
-            }
-
-            folders[folderPath] = BookmarkTreeNode.id;
-            pathLookup[BookmarkTreeNode.id + ""] = folderPath;
-            populateBookmarkOptions(children, depth + 1, folders, folderPath);
-        }
+  if (depth === 0) {
+    var options = "";
+    for (tFolder in folders) {
+      var folderId = folders[tFolder];
+      if (tFolder === "") {
+        tFolder = "(root)";
+      }
+      options += "<option value='" + folderId + "'>" + tFolder + "</option>";
     }
-
-    if(depth === 0) {
-        var options = "";
-        for(tFolder in folders) {
-            var folderId = folders[tFolder];
-            if(tFolder === "") {
-                tFolder = "(root)";
-            }
-            options += "<option value='" + folderId + "'>" + tFolder + "</option>";
-        }
-        document.getElementById("folder-select").innerHTML = options;
-    }
+    document.getElementById("folder-select").innerHTML = options;
+  }
 };
 
 chrome.bookmarks.getTree(populateBookmarkOptions);
 
-function exportBookmarkCallback(BookmarkTreeNodeArray, depth, entries, folderLabel) {
-
-    if(typeof depth === 'undefined') {
-        depth = 0;
-        entries = [];
-        folderLabel = pathLookup[selectedId];
-    }
-
-    for(var x=0; x<BookmarkTreeNodeArray.length; x++) {
-        var BookmarkTreeNode = BookmarkTreeNodeArray[x];
-        var type = getBookmarkNodeType(BookmarkTreeNode);
-
-        if(type === "Folder") {
-            var children = BookmarkTreeNode.children;
-            //Extension.log("Folder: " + BookmarkTreeNode.title + ", " + children.length + " items");
-
-            if(depth === 0) {
-                folderPath = folderLabel;
-            } else {
-                folderPath = folderLabel + "/" + BookmarkTreeNode.title;
-            }
-
-            exportBookmarkCallback(children, depth + 1, entries, folderPath);
-        } else {
-            //Extension.log("Bookmark: " + BookmarkTreeNode.title + ", " + BookmarkTreeNode.url);
-            json = {
-                "name": BookmarkTreeNode.title,
-                "url": BookmarkTreeNode.url,
-                "path": folderLabel
-            };
-            entries.push(json);
-        }
-    }
-
-    if(depth === 0) {
-        var outputElem = document.getElementById('output');
-        outputElem.style.height = "10px";
-
-        outputElem.innerText = JSON.stringify(entries, null, '\t');
-        outputElem.style.height = (outputElem.scrollHeight)+"px";
-    }
-};
-
 function exportBookmarks(e) {
-    e.preventDefault();
-    var selectControl = document.getElementById("folder-select");
-    var controlSelectedId = selectControl.options[selectControl.selectedIndex].value;
-    selectedId = controlSelectedId;
+  e.preventDefault();
+  var selectControl = document.getElementById("folder-select");
+  var controlSelectedId = selectControl.options[selectControl.selectedIndex].value;
+  const selectedId = controlSelectedId;
 
-    chrome.bookmarks.getSubTree(selectedId, exportBookmarkCallback);
+  chrome.bookmarks.getSubTree(selectedId, exportBookmarkCallback);
 }
 
-window.onload = function() {
-    document.getElementById("export-form").onsubmit = exportBookmarks;
+function inRange(number) {
+  if (number === 0) return '#'
+  let s = ''
+  for (let i = 0; i < number; i++) {
+    s += '#'
+  }
+  return s
+}
+
+function exportBookmarkCallback(BookmarkTreeNodeArray, depth, entries, folderLabel) {
+
+  if (typeof depth === 'undefined') {
+    depth = 0;
+    entries = [];
+    folderLabel = pathLookup[selectedId] ? pathLookup[selectedId] : null;
+    console.info(BookmarkTreeNodeArray)
+  }
+
+  BookmarkTreeNodeArray.forEach(bookTreeNode => {
+    var type = getBookmarkNodeType(bookTreeNode);
+    if (type === "Folder") {
+      var children = bookTreeNode.children;
+      //Extension.log("Folder: " + bookTreeNode.title + ", " + children.length + " items");
+
+      if (folderLabel || bookTreeNode.title) {
+        const title = depth === 0 && folderLabel ? `# ${folderLabel}` : `${inRange(depth)} ${bookTreeNode.title}`
+        entries.push(title)
+      }
+
+      if (depth === 0) {
+        folderPath = folderLabel;
+      } else {
+        folderPath = folderLabel + "/" + bookTreeNode.title;
+      }
+
+      exportBookmarkCallback(children, depth + 1, entries, folderPath);
+    } else if (type === 'Bookmark') {
+      const md = `[${bookTreeNode.title}](${bookTreeNode.url})`
+      entries.push(md);
+    }
+  })
+
+  if (depth === 0) {
+    var outputElem = document.getElementById('output');
+    outputElem.style.height = "10px";
+    let string = ''
+    entries.forEach(e => {
+      string += e + '\r\n'
+    })
+
+    outputElem.innerText = string;
+    outputElem.style.height = (outputElem.scrollHeight) + "px";
+  }
+};
+
+window.onload = function () {
+  document.getElementById("export-form").onsubmit = exportBookmarks;
 }
